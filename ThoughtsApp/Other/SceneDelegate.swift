7026d5979dfde0e089f37c37569abd31fdbb43c8
Paulo Filho <pcf.feat@gmail.com>
2022-09-16 14:08:07 -0400

    display screens based on user being signed in

--- ThoughtsApp/Other/SceneDelegate.swift
+++ ThoughtsApp/Other/SceneDelegate.swift
@@ -16,8 +16,19 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         let window = UIWindow(windowScene: windowScene)
         
-        // TODO: Update VC to sign in VC if not signed in
-        let vc = TabBarViewController()
+        let vc: UIViewController
+        if AuthManager.shared.isSignedIn {
+            vc = TabBarViewController()
+        } else {
+            let signInVC = SignInViewController()
+            signInVC.navigationItem.largeTitleDisplayMode = .always
+            
+            let navVC = UINavigationController(rootViewController: signInVC)
+            navVC.navigationBar.prefersLargeTitles = true
+            
+            vc = navVC
+        }
+        
         window.rootViewController = vc
         window.makeKeyAndVisible()
         self.window = window
