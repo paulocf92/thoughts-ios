2da19f3055930f38270b13c526e395b65d1c6c39
Paulo Filho <pcf.feat@gmail.com>
2022-09-16 14:08:07 -0400

    create CTA buttons for paywall screen

--- ThoughtsApp/Controllers/Other/PayWallViewController.swift
+++ ThoughtsApp/Controllers/Other/PayWallViewController.swift
@@ -13,13 +13,38 @@ class PayWallViewController: UIViewController {
     
     // Pricing and product info
     // CTA Buttons
+    private let buyButton: UIButton = {
+       let button = UIButton()
+        button.setTitle("Subscribe", for: .normal)
+        button.backgroundColor = .systemBlue
+        button.setTitleColor(.white, for: .normal)
+        button.layer.cornerRadius = 8
+        button.layer.masksToBounds = true
+        
+        return button
+    }()
+    
+    private let restoreButton: UIButton = {
+       let button = UIButton()
+        button.setTitle("Restore Purchases", for: .normal)
+        button.setTitleColor(.link, for: .normal)
+        button.layer.cornerRadius = 8
+        button.layer.masksToBounds = true
+        
+        return button
+    }()
+    
     // Terms of Service
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        title = "Thoughts Premium"
         view.backgroundColor = .systemBackground
         view.addSubview(header)
+        view.addSubview(buyButton)
+        view.addSubview(restoreButton)
         setUpCloseButton()
+        setUpButtons()
     }
     
     override func viewDidLayoutSubviews() {
@@ -30,6 +55,19 @@ class PayWallViewController: UIViewController {
             width: view.width,
             height: view.height/3.2
         )
+    }
+    
+    private func setUpButtons() {
+        buyButton.addTarget(self, action: #selector(didTapSubscribe), for: .touchUpInside)
+        restoreButton.addTarget(self, action: #selector(didTapRestore), for: .touchUpInside)
+    }
+    
+    @objc private func didTapSubscribe () {
+        
+    }
+    
+    @objc private func didTapRestore () {
+        
     }
     
     private func setUpCloseButton() {
