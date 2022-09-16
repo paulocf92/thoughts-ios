feb0b1b200429ce70f3ce1840fabb41db984e654
Paulo Filho <pcf.feat@gmail.com>
2022-09-16 14:08:06 -0400

    set up initial database manager layout

--- ThoughtsApp/Managers/DatabaseManager.swift
+++ ThoughtsApp/Managers/DatabaseManager.swift
@@ -14,4 +14,35 @@ final class DatabaseManager {
     private let database = Firestore.firestore()
     
     private init() {}
+    
+    public func insertBlogPost(
+        with post: String,
+        user: String,
+        completion: @escaping (Bool) -> Void
+    ) {
+        
+    }
+    
+    public func getAllPosts(
+        with post: String,
+        user: String,
+        completion: @escaping ([String]) -> Void
+    ) {
+        
+    }
+    
+    public func getPostsForUser(
+        with post: String,
+        user: String,
+        completion: @escaping ([String]) -> Void
+    ) {
+        
+    }
+    
+    public func insertUser(
+        user: String,
+        completion: @escaping (Bool) -> Void
+    ) {
+        
+    }
 }
