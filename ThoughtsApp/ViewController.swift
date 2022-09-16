6d581ecbdf4cbc30d27533af377f68753ed13b44
Paulo Filho <pcf.feat@gmail.com>
2022-09-16 14:08:05 -0400

    Initial Commit

---
+++ ThoughtsApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ThoughtsApp
+//
+//  Created by Paulo Filho on 22/08/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
