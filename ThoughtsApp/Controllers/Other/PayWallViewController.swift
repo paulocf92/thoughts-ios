a29d99120bb8b36963f7d89f02781c21e4e9e38f
Paulo Filho <pcf.feat@gmail.com>
2022-09-16 14:08:07 -0400

    draft paywall view

---
+++ ThoughtsApp/Controllers/Other/PayWallViewController.swift
@@ -0,0 +1,24 @@
+//
+//  PayWallViewController.swift
+//  ThoughtsApp
+//
+//  Created by Paulo Filho on 25/08/22.
+//
+
+import UIKit
+
+class PayWallViewController: UIViewController {
+    
+    // Close button / Title
+    // Header Image
+    // Pricing and product info
+    // CTA Buttons
+    // Terms of Service
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemBackground
+    }
+
+}
