0fda5d5c410a77ba3d38e365cee0d14db9c0c4b5
Paulo Filho <pcf.feat@gmail.com>
2022-09-16 14:08:07 -0400

    add compose post button

--- ThoughtsApp/Controllers/Core Tabs/HomeViewController.swift
+++ ThoughtsApp/Controllers/Core Tabs/HomeViewController.swift
@@ -8,12 +8,38 @@
 import UIKit
 
 class HomeViewController: UIViewController {
+    
+    private let composeButton: UIButton = {
+        let button = UIButton()
+        button.backgroundColor = .systemBlue
+        button.tintColor = .white
+        button.setImage(
+            UIImage(
+                systemName: "square.and.pencil",
+                withConfiguration: UIImage.SymbolConfiguration(pointSize: 32, weight: .medium)
+            ), for: .normal)
+        button.layer.cornerRadius = 40
+        button.layer.shadowColor = UIColor.label.cgColor
+        button.layer.shadowOpacity = 0.4
+        button.layer.shadowRadius = 10
+        
+        return button
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
-        
-        
+        view.addSubview(composeButton)
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        composeButton.frame = CGRect(
+            x: view.frame.width - 80 - 16,
+            y: view.frame.height - 80 - 16 - view.safeAreaInsets.bottom,
+            width: 80,
+            height: 80
+        )
     }
 
 }
