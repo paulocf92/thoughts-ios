f392b85b469e2b3cb6bf0d17595388135e75f419
Paulo Filho <pcf.feat@gmail.com>
2022-09-16 14:08:08 -0400

    create post preview cell to hold data related to a single post

---
+++ ThoughtsApp/Views/Cells/PostHeaderTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  PostHeaderTableViewCell.swift
+//  ThoughtsApp
+//
+//  Created by Paulo Filho on 30/08/22.
+//
+
+import UIKit
+
+class PostHeaderTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
